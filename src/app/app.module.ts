d031650582b0ef497ada18e0f98599188792fdd6
Isha Saxena <ishasaxena1994@gmail.com>
2021-02-03 11:47:43 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckoutPageComponent } from './component/checkout-page/checkout-page.component';
+import { DetailPageComponent } from './component/checkout-page/detail-page/detail-page.component';
+import { PaymentPageComponent } from './component/checkout-page/payment-page/payment-page.component';
+import { SuccessPageComponent } from './component/checkout-page/success-page/success-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckoutPageComponent,
+    DetailPageComponent,
+    PaymentPageComponent,
+    SuccessPageComponent
   ],
   imports: [
     BrowserModule,
